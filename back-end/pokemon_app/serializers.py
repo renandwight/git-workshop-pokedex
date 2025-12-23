af6a01528ea12aea9dcecd375749fcb4ec66e7e0
rvillarrealcp <richard.villarreal2@hotmail.com>
2025-12-23 14:00:26 -0600

    Move Serializer Added

--- back-end/pokemon_app/serializers.py
+++ back-end/pokemon_app/serializers.py
@@ -1,9 +1,23 @@
 # pokemon_app/serializers.py
 from rest_framework import serializers # import serializers from DRF
 from .models import Pokemon # import Pokemon model from models.py
+from .models import Move
 
 class PokemonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Pokemon # specify what model this serializer is for
         fields = ['id', 'name', 'level'] # specify the fields you would like this serializer to return
+
+# move_app/serializers.py
+class MoveSerializer(serializers.ModelSerializer):
+    pokemon = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Move
+        fields = ['id', 'name', 'power', 'accuracy', 'pokemon']
+
+    def get_pokemon(self, obj):
+        pokemon = obj.pokemon.all()
+        pokemon = [x.name for x in pokemon]
+        return pokemon
