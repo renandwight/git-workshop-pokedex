4942d5eb8adfa118e89def2df7c7ae4ce89093bd
rvillarrealcp <richard.villarreal2@hotmail.com>
2025-12-23 14:40:41 -0600

    added create_all_moves_route_view

--- back-end/moves_app/views.py
+++ back-end/moves_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .serializers import MoveSerializer
 
-# Create your views here.
+class All_moves(APIView):
+    # establish a get method that will be triggered by GET requests
+    def get(self, request):
+        # utilize your ModelSerializer to serialize your queryset and return a proper response with DRF's Response
+        moves = MoveSerializer(Move.objects.all(), many=True)
+        return Response(moves.data)
